97e116ace406ef26a531ba38d782f0e5100d3a84
Boris Kamber Garcevic <G82024@velliv.dk>
2025-04-07 21:44:23 +0200

    Update .gitignore and add TMDL parser architecture documentation

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,6 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-    console.log('TMDL Studio extension is now active');
-
-    // Register language features
     const disposable = vscode.languages.registerDocumentFormattingEditProvider('tmdl', {
         provideDocumentFormattingEdits(document: vscode.TextDocument): vscode.TextEdit[] {
             // Basic formatting placeholder
